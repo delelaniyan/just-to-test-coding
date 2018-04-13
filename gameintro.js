cd4baa8c2c09e25ce7bbb5e07b1bc0ee0eb18af4
delelaniyan <36045663+delelaniyan@users.noreply.github.com>
2018-04-13 11:03:28 -0400

    Create gameintro.js

---
+++ gameintro.js
@@ -0,0 +1,41 @@
+#Video Game Design:
+
+//Intro:
+function setup() {
+  createCanvas(500, 500);
+noStroke()
+}
+
+var x = 0
+var y = 0
+
+
+
+function draw() {
+  background(64, 137, 232);
+	
+  //Logo
+  x = 200
+	y = 200
+	fill(255, 178, 0);
+	ellipse(250, 250, x, y );
+	
+  //text
+	fill(0, 0, 0);
+	textSize(50);
+	text("Logo", 195, 250);
+}
+
+//Amanuels part below
+class Rectangle {
+float left;
+float right;
+float top; 
+float bottom;
+
+Rectangle(float x, float y, float w, float  h) {
+	left x;
+	right = x + w;
+	top y;
+	bottom = y + h
+}	
